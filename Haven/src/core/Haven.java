c587c158e07f92830fda9fea59a9a6e69e18129a
Jochen Joswig <6joswig@informatik.uni-hamburg.de>
2016-05-20 11:46:39 +0200

    static Server implemented for testing.

--- Haven/src/core/Haven.java
+++ Haven/src/core/Haven.java
@@ -1,9 +1,38 @@
 package core;
 
+import java.net.InetAddress;
+import org.glassfish.grizzly.http.server.HttpServer;
+
+import com.sun.jersey.api.container.grizzly2.GrizzlyServerFactory;
+
 public class Haven {
+	
+	private final static int HTTP_PORT = 8080;
+    private final static int PING_PORT = 9999;  // port to ping the clients at
+    static String localIP;               // server ip
+    static HttpServer server;
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-	}
 
+		try {
+			startServer();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+    static void startServer() throws Exception{
+    	
+    	// port for the HTTP connection
+     	localIP = InetAddress.getLocalHost().getHostAddress();
+        String url = "http://" + localIP + ":" + HTTP_PORT;
+
+        server = GrizzlyServerFactory.createHttpServer(url);
+
+        System.out.println( "URL: " + url );
+    	
+    }
 }
