e50ccb6721c61156e7cc2260d6931245f8a037de
Jochen Joswig <6joswig@informatik.uni-hamburg.de>
2016-05-20 13:43:14 +0200

    Haven fixed so that it does not immediately close.

--- Haven/src/core/Haven.java
+++ Haven/src/core/Haven.java
@@ -1,5 +1,9 @@
 package core;
 
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.io.Console;
 import java.net.InetAddress;
 import org.glassfish.grizzly.http.server.HttpServer;
 
@@ -11,9 +15,11 @@ public class Haven {
     private final static int PING_PORT = 9999;  // port to ping the clients at
     static String localIP;               // server ip
     static HttpServer server;
+    static Timer timer;
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		Scanner scanner = new Scanner(System.in);
 
 		try {
 			startServer();
@@ -21,7 +27,16 @@ public class Haven {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+		
+		timer = new Timer();
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				System.out.println("Running...");
+			}
+		}, 10000, 10000);
+		System.out.println("Please, enter any number to exit.");
+		int i = scanner.nextInt();
 	}
 	
     static void startServer() throws Exception{
