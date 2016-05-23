1df66a2e44511a001166077364440499801467f7
Kim Reichert <kimrdot@gmail.com>
2016-05-23 12:28:21 +0200

    Import Logging log4j and configured it to log to logs/Haven.log as well as the console. Please only use logger from now ont (not system.out)

--- Haven/src/core/Haven.java
+++ Haven/src/core/Haven.java
@@ -5,47 +5,47 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.io.IOException;
 import java.net.InetAddress;
+
+import org.apache.logging.log4j.*;
 import org.glassfish.grizzly.http.server.HttpServer;
 
 import com.sun.jersey.api.container.grizzly2.GrizzlyServerFactory;
 
+import resourcerer.DockerResource;
 import resourcerer.Resourcerer;
 
 public class Haven {
-	
+
+	/* Get actual class name to be printed on */
+	static final Logger logger = LogManager.getLogger(Haven.class.getName());
+
 	private final static int HTTP_PORT = 8080;
-    private final static int PING_PORT = 9999;  // port to ping the clients at
-    static String localIP;               // server ip
-    static HttpServer server;
-    static Timer timer;
+	private final static int PING_PORT = 9999; // port to ping the clients at
+	static String localIP; // server ip
+	static HttpServer server;
+	static Timer timer;
 
-	public static void main(String[] args) {
-		System.out.println("Starting Haven");
+	public static void main(String[] args) throws IOException {
+		logger.info("Starting Haven");
+
+		// start Resourcer
+		logger.info("Starting Resourcerer");
 		
-		
-		//start Resourcer
-		System.out.println("Starting Resourcerer");
 		Resourcerer schmendrik = new Resourcerer();
-		try {
-			String result = schmendrik
-					.getJsonFrom("");
+		String result = schmendrik.getJsonFrom(schmendrik.getAttachedResource(""));
+		logger.debug(result);
 
-			System.out.print(result);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		//Start Scanner
-		System.out.println("Starting Scanner");
+		// Start Scanner
+		logger.info("Starting Scanner");
 		Scanner scanner = new Scanner(System.in);
 
 		try {
 			startServer();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			logger.error("Starting ApiServer failed with " + e);
 		}
-		
+
 		timer = new Timer();
 		timer.schedule(new TimerTask() {
 			@Override
@@ -55,20 +55,19 @@ public class Haven {
 		}, 10000, 10000);
 		System.out.println("Please, enter 0 to exit.");
 		int i = scanner.nextInt();
-		if(i == 0){
+		if (i == 0) {
 			timer.cancel();
 		}
 	}
-	
-    static void startServer() throws Exception{
-    	
-    	// port for the HTTP connection
-     	localIP = InetAddress.getLocalHost().getHostAddress();
-        String url = "http://" + localIP + ":" + HTTP_PORT;
 
-        server = GrizzlyServerFactory.createHttpServer("http://0.0.0.0:8080");
+	static void startServer() throws Exception {
 
-        System.out.println( "URL: " + url );
-    	
-    }
+		// port for the HTTP connection
+		localIP = InetAddress.getLocalHost().getHostAddress();
+		String url = "http://" + localIP + ":" + HTTP_PORT;
+
+		server = GrizzlyServerFactory.createHttpServer("http://0.0.0.0:8080");
+
+		logger.info("Current URL is : " + url);
+	}
 }
--- Haven/src/resourcerer/Resourcerer.java
+++ Haven/src/resourcerer/Resourcerer.java
@@ -7,14 +7,33 @@ import java.util.List;
 
 public class Resourcerer {
 
-	private List<URL> resources;
+	private List<AbstractResource> resources;
 
-	public Resourcerer(URL... resourceUrls) {
-		resources = new ArrayList<URL>();
-		for (URL url: resourceUrls)
+	public Resourcerer(AbstractResource... resourcesToWatch) 
+	{
+		
+		resources = new ArrayList<AbstractResource>();
+		for (AbstractResource resource: resourcesToWatch)
 		{
-			resources.add(url);
+			resources.add(resource);
 		}
+		
+		try {
+			resources.add(new DockerResource());
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String getJsonFrom(AbstractResource resource)
+	{
+		String result = "";
+		try {
+			result = getJsonFrom(resource.getResourceUrl().toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 
 	public String getJsonFrom(String urlToRead) throws IOException {
@@ -29,5 +48,10 @@ public class Resourcerer {
 		}
 		rd.close();
 		return result.toString();
+	}
+	
+	public AbstractResource getAttachedResource(String name)
+	{
+		return resources.get(0);
 	}
 }
